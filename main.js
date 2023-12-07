1824fbcefc7e26656854cc3da692657475200c38
XTruckDriver <120071320+XTruckDriver@users.noreply.github.com>
2023-12-07 07:21:43 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// declaring variable and assigning my age
+const myAge = 42;
+// declare variable and assign value of 2/ 
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Mark';
+myName = myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
